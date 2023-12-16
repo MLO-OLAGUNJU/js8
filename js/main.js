ed9bc59001bd59d06cc28f5e898b8175d434ac39
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-16 16:17:37 +0100

    learning javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// conditionals: Tenary Operators
+
+//Syntax
+//condition ? ifTrue : ifFalse;
+
+let soup = "Chicken noodle soup";
+let response = soup ? "Yes, we have soup." : "Sorry, no soup today.";
+
+console.log(response);
